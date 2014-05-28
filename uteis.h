2112e228ad80e07f72eff0daa456296971b3457e
zezebyte <zeze_byte@hotmail.com>
2014-05-28 22:01:46 +0100

    Create uteis.h

---
+++ uteis.h
@@ -0,0 +1,24 @@
+/*
+ Este Vai ser o principal HeaderFile
+ 
+ */
+#ifndef _UTEISH_
+#define _UTEISH_
+
+#include <stdio.h>
+#include <stdlib.h>
+#define STRG 80
+#define MAX 100
+void limparEcra(){ //funcao para limpar o ecra
+    system(" cmd /c cls ");
+}
+
+void Prima(){ //funcao de espera
+    printf("Prima ENTER para continuar...\n");
+    getchar();
+}
+
+
+
+
+#endif
